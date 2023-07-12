615b30be7d347c233b0e8c6e415e6edddd7cc3ef
Kristijan Lazarev <133780727+lazarevkristijan@users.noreply.github.com>
2023-07-12 17:11:30 +0200

    fixed small typo

--- app/project/[id]/page.tsx
+++ app/project/[id]/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 import { getCurrentUser } from "@/lib/session"
 import { getProjectDetails } from "@/lib/actions"
 import Modal from "@/components/Modal"
-import ProjectActions from "components/ProjectActions"
+import ProjectActions from "@/components/ProjectActions"
 import RelatedProjects from "@/components/RelatedProjects"
 import { ProjectInterface } from "@/common.types"
 
